48de9264a705a993df8670d2290eb50cc272b742
Jacob Marshall <jacob.marshall.jem@gmail.com>
2022-05-20 19:41:20 +0000

    🗃️ Prisma for auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,11 @@
 // pages/api/auth/[...nextauth.js]
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import prisma from "../../../lib/prisma";
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   debug: false,
   jwt: {
