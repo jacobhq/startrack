fb7db7789bc4f572c0dd360dc656cae34d069ddb
Jacob Marshall <jacob.marshall.jem@gmail.com>
2022-05-20 19:25:25 +0000

    🚧 API star changes

--- pages/api/star/[...slug].ts
+++ pages/api/star/[...slug].ts
@@ -1,16 +1,22 @@
 import { Octokit } from "@octokit/rest";
 import axios from 'axios';
 import { useState } from 'react';
+import { getToken } from "next-auth/jwt"
+
+const secret = process.env.SECRET
 
 export default async (req, res) => {
   const { slug } = req.query
-  let token
+  const token = await getToken({ req, secret })
+  let data
 
-  await axios.get('/api/getToken').then(function (response) {
-    console.log(slug)
+  await axios.get('http://localhost:3000/api/getToken', {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  }).then(function (response) {
+    data = response.data
     res.send(slug, response.data.token)
-    token = response.data.token
-
     axios.put('https://api.github.com/user/starred/' + slug[0] + '/' + slug[1], {
       headers: {
         'Authorization': `Bearer ${response.data.token}`
@@ -18,8 +24,8 @@ export default async (req, res) => {
     })
   }).catch(function (error) {
     // handle error
-    res.send(error)
+    console.log(error.message)
   })
 
-  res.end(slug.toString(), token);
+  res.end(slug.toString(), data);
 };
