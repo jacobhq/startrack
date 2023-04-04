bf305816f5735fb6e5e5936111675a0547d8f88c
Jacob Marshall <jacob@omg.lol>
2023-04-04 13:41:54 +0000

    🚑 Fix local dev (node v16 reqired for prisma)

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import EmailProvider from "next-auth/providers/email";
 import prisma from "lib/prisma";
 
 export default NextAuth({
@@ -18,6 +19,10 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
       authorization: { params: { scope: 'public_repo user:follow' } },
     }),
+    // EmailProvider({
+    //   server: process.env.EMAIL_SERVER,
+    //   from: process.env.EMAIL_FROM,
+    // })
     // ...add more providers here
   ],
   callbacks: {
