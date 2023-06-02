e21f9f6f1e65ffcc1dccfa264b6a36546f70a35b
Jacob Marshall <me@jhqcat.com>
2023-06-02 23:41:35 +0100

    ⚰️ Remove old chakra code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,9 @@
-import { ChakraProvider, extendTheme } from "@chakra-ui/react"
 import '../styles/globals.css'
-import { StepsStyleConfig as Steps } from 'chakra-ui-steps';
 import { SessionProvider } from "next-auth/react"
 import { useEffect } from "react";
 import splitbee from '@splitbee/web';
 import { Analytics } from '@vercel/analytics/react';
 import { Toaster } from "react-hot-toast";
-
-const theme = extendTheme({
-  initialColorMode: "light",
-  useSystemColorMode: false,
-  components: {
-    Steps,
-  },
-});
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   useEffect(() => {
